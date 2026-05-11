bdae4926594e94d72260c41f6f66a5e1854ceb31
Mirxsa <miradelacruzgomez@gmail.com>
2026-05-11 10:09:10 +0800

    Change cards per slide to 3 and adjust card width

--- slider.js
+++ slider.js
@@ -3,11 +3,11 @@ const next = document.getElementById('next');
 const prev = document.getElementById('prev');
 
 let currentIndex = 0;
-const cardsPerSlide = 4;
+const cardsPerSlide = 3;
 const totalCards = slider.children.length;
 
 
-const cardWidth = 250 + 30; 
+const cardWidth = 250 + 32; 
 
 next.addEventListener('click', () => {
   if (currentIndex + cardsPerSlide < totalCards) {
